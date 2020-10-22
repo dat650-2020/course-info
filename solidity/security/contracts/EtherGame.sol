01f82a1ea7642076c7dcaf7a68d48511f8bf53d4
Rodrigo Q. Saramago <rodrigoqsaramago@gmail.com>
2020-10-22 10:16:52 +0200

    Fix coding formating and lint errors

--- solidity/security/contracts/EtherGame.sol
+++ solidity/security/contracts/EtherGame.sol
@@ -1,29 +1,28 @@
+// SPDX-License-Identifier: MIT
 pragma solidity 0.5.11;
 
 contract EtherGame {
+    uint256 public payoutMileStone1 = 3 ether;
+    uint256 public mileStone1Reward = 2 ether;
+    uint256 public payoutMileStone2 = 5 ether;
+    uint256 public mileStone2Reward = 3 ether;
+    uint256 public finalMileStone = 10 ether;
+    uint256 public finalReward = 5 ether;
 
-    uint public payoutMileStone1 = 3 ether;
-    uint public mileStone1Reward = 2 ether;
-    uint public payoutMileStone2 = 5 ether;
-    uint public mileStone2Reward = 3 ether;
-    uint public finalMileStone = 10 ether;
-    uint public finalReward = 5 ether;
+    mapping(address => uint256) redeemableEther;
 
-    mapping(address => uint) redeemableEther;
     // Users pay 0.5 ether. At specific milestones, credit their accounts.
     function play() public payable {
         require(msg.value == 0.5 ether); // each play is 0.5 ether
-        uint currentBalance = address(this).balance + msg.value;
+        uint256 currentBalance = address(this).balance + msg.value;
         // ensure no players after the game has finished
         require(currentBalance <= finalMileStone);
         // if at a milestone, credit the player's account
         if (currentBalance == payoutMileStone1) {
             redeemableEther[msg.sender] += mileStone1Reward;
-        }
-        else if (currentBalance == payoutMileStone2) {
+        } else if (currentBalance == payoutMileStone2) {
             redeemableEther[msg.sender] += mileStone2Reward;
-        }
-        else if (currentBalance == finalMileStone ) {
+        } else if (currentBalance == finalMileStone) {
             redeemableEther[msg.sender] += finalReward;
         }
         return;
@@ -34,8 +33,8 @@ contract EtherGame {
         require(address(this).balance == finalMileStone);
         // ensure there is a reward to give
         require(redeemableEther[msg.sender] > 0);
-        uint transferValue = redeemableEther[msg.sender];
+        uint256 transferValue = redeemableEther[msg.sender];
         redeemableEther[msg.sender] = 0;
         msg.sender.transfer(transferValue);
     }
- }
+}
